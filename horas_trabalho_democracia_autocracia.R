f67079e99c11296264ce30bf7926a14e417717fa
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-12-01 23:15:14 -0300

    Add files via upload

--- horas_trabalho_democracia_autocracia.R
+++ horas_trabalho_democracia_autocracia.R
@@ -7,6 +7,15 @@
 # Sobre os dados ---------------------------------------------------------------------------------------------------------------------------
 
 ### Trabalho é uma parte central de nossas vidas. É algo que nós fazemos quase todos os
-### dias, por muitos dias, e por décadas. 
+### dias, por muitos dias, e por décadas. Por ser uma questão central, observar de perto
+### quanto tempo nós gastamos trabalhando pode nos dizer muito sobre nossas vidas e sobre
+### a sociedade em que vivemos.
 
+### O quanto as pessoas no mundo trabalham? Em quantos países hoje as pessoas trabalham
+### muito menos que nos passados 150 anos? Trabalhar menos significa ser capaz de gastar
+### tempo se tornando mais educado, ou simplesmentes tendo mais tempo de lazer. Isso é um
+### substancial progresso, mas existem ainda muitas desigualdades entre e dentro de países.
+
+### Aqui nós apresentamos dados sobre horas de trabalho. Nós exploramos como isso ocorre
+### nos países e ao longo do tempo, e como essas diferenças importam na vida das pessoas.
 
